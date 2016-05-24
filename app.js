5373044062b7a3a5efaacfc16e3d0a3faba6cefb
Tadd Giles <tadd@taddgiles.com>
2016-05-24 14:40:46 -0600

    Move to postgres

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 var express = require('express')
 var bodyParser = require('body-parser')
-var mongoose = require('mongoose')
-var User = require('./user')
-var bcrypt = require('bcrypt')
 var jwt = require('jwt-simple')
 var secret = process.env.JWT_SECRET || 'secret'
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/platform-test')
+var pg = require('knex')({
+  client: 'pg',
+  connection: process.env.DATABASE_URL || "postgres://localhost/platform_test_development",
+  searchPath: 'knex,public'
+})
 
 var app = express()
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -15,8 +16,6 @@ app.use(bodyParser.json())
 var router = express.Router()
 
 router.use((req, res, next) => {
-  if (req.originalUrl.match(/authenticate$/)) return next()
-
   var header = req.get('Authorization') || ''
   var token = header.split(' ')[1]
 
@@ -28,62 +27,22 @@ router.use((req, res, next) => {
     return res.status(401).json(ex.message)
   }
 
-  User.findById(userId, function(err, user) {
-    if (err) return res.json(err)
-    if (!user) return res.status(401).json()
-
-    req.user = user
+  pg('users').where({
+    id: userId
+  })
+  .then((rows) => {
+    if (rows.length === 0) {
+      return res.status(401).json()
+    }
+    console.log(rows[0])
+    delete rows[0].password_digest
+    req.user = rows[0]
     next()
   })
 })
 
-router.route('/authenticate')
-  .post((req, res) => {
-    User.findOne({ email: req.body.email }, (err, user) => {
-      if (err) return res.json(err)
-      if (!user) return res.status(404).json()
-
-      console.log('USER', user)
-      if (!bcrypt.compareSync(req.body.password, user.password_digest)) {
-        return res.status(401).json({ message: 'Invalid password' })
-      }
-
-      var payload = { user_id: user._id }
-      res.json({ authToken: jwt.encode(payload, secret) })
-    })
-  })
-
 router.route('/current')
   .get((req, res) => { res.json(req.user) })
-
-router.route('/')
-  .get(function(req, res) {
-    User.find(function(err, users) {
-      if (err) return res.json(err)
-      res.json(users)
-    })
-  })
-  .post(function(req, res) {
-    var user = new User()
-    user.name = req.body.name
-    user.email = req.body.email
-    if (req.body.password) {
-      user.password = req.body.password
-    }
-
-    user.save(function(err) {
-      if (err) return res.json(err)
-      res.json(user)
-    })
-  })
-
-router.route('/:id')
-  .get(function(req, res) {
-    User.findById(req.params.id, function(err, user) {
-      if (err) return res.json(err)
-      res.json(user)
-    })
-  })
 
 app.use('/api/v1/users', router)
 
